e773bcbecc010e7acb4d5b29b63aa5b0c553dd85
Derrick Morton <morton.derrick@gmail.com>
2016-03-24 01:43:04 -0400

    Adding album controller

---
+++ app/scripts/controllers/AlbumCtrl.js
@@ -0,0 +1,26 @@
+(function() {
+    function AlbumCtrl() {
+        this.albumData = angular.copy(albumPicasso);
+        this.albumData.push(angular.copy(albumPicasso));
+
+    }
+
+    angular
+        .module('blocJams')
+        .controller('AlbumCtrl', AlbumCtrl);
+})();
+
+
+// Create a controller for the Album view.
+// Link to the AlbumCtrl.js script source in index.html.
+// Add an albumData property that holds a copy of albumPicasso.
+// Use ngRepeat on the album-view-song-item table row to add a song row for each song on the album.
+// Replace the static song information with the song data using the corresponding scope properties and markup:
+// number (Refer to the table of "exposed properties" in the ngRepeat documentation)
+// name
+// length (You'll filter the time code in a later checkpoint)
+// In the Album template, replace the static album information with the album data using markup:
+// album art
+// name
+// artist
+// year and record label
