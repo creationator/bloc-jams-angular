20b9dc1b021427185e9454c078f5f828ac2ca5f0
Derrick Morton <morton.derrick@gmail.com>
2016-03-31 23:58:51 -0400

    Services part 1 completed

--- dist/scripts/controllers/AlbumCtrl.js
+++ dist/scripts/controllers/AlbumCtrl.js
@@ -1,13 +1,8 @@
 (function() {
-    function AlbumCtrl() {
-        this.albumData = [];//delete
-        for (var i=0; i < 5; i++) {
-          // this.albumData.push(angular.copy(albumPicasso)); delete
-          this.albumData = angular.copy(albumPicasso);
-          console.log(this.albumData);
-        }
+    function AlbumCtrl(Fixtures) {
+          this.albumData = Fixtures.getAlbum();
     }
     angular
         .module('blocJams')
-        .controller('AlbumCtrl', AlbumCtrl);
+        .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
 })();
